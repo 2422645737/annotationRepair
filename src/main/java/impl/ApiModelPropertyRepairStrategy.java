2e263ec0ecdcff665724e181f52bc49f6cb27a48
wanghui <wanghui4@musnhealth.com>
2024-06-04 11:58:41 +0800

    处理注释中结尾处包含空格的问题

--- src/main/java/impl/ApiModelPropertyRepairStrategy.java
+++ src/main/java/impl/ApiModelPropertyRepairStrategy.java
@@ -41,18 +41,18 @@ public class ApiModelPropertyRepairStrategy extends AbstractRegexMatcher impleme
 //            }
 //        }
 
-        //处理没有注释并且前面带有注解的参数
-        String compile = "[\\t|\\s*]@.*(((\\r\\n\\s*.*?)+)private\\s*([a-z]|[A-Z]|[0-9]|>|<|_)*\\s*(([a-z]|[A-Z]|[0-9]|>|<|_)*);)";
-        Matcher matcher = Pattern.compile(compile).matcher(replace);
-        while(matcher.find()){
-            String group = matcher.group();
-            String comment = Pattern.compile(compile).matcher(group).replaceAll("$5");
-            System.out.println(group);
-            if(!group.contains("ApiModelProperty")){
-                String replace1 = group.replace("private", "@ApiModelProperty(value = \"" + comment + "\")\n\tprivate");
-                replace = replace.replace(group,replace1);
-            }
-        }
+        //处理没有注释并且前面带有注解的参数（如果需要跳过注解修复ApiModelProperty，把这段代码放开，不然很影响效率）
+//        String compile = "[\\t|\\s*]@.*(((\\r\\n\\s*.*?)+)private\\s*([a-z]|[A-Z]|[0-9]|>|<|_)*\\s*(([a-z]|[A-Z]|[0-9]|>|<|_)*);)";
+//        Matcher matcher = Pattern.compile(compile).matcher(replace);
+//        while(matcher.find()){
+//            String group = matcher.group();
+//            String comment = Pattern.compile(compile).matcher(group).replaceAll("$5");
+//            System.out.println(group);
+//            if(!group.contains("ApiModelProperty")){
+//                String replace1 = group.replace("private", "@ApiModelProperty(value = \"" + comment + "\")\n\tprivate");
+//                replace = replace.replace(group,replace1);
+//            }
+//        }
 
         writeFile(replace,file);
     }
